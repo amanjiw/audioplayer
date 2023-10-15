c3146710e3fd56e44615a9ee9c951d9097a9194e
amanj <a.ghaderi@ria.co.ir>
2023-10-15 18:58:28 +0330

    play next and prev track logic added

---
+++ src/audioplayer/hooks.ts
@@ -0,0 +1,39 @@
+import { useRef, useState, useEffect } from "react";
+import { createAudioPlayer } from "./Audioplayer";
+import { Controls, PlayerState, Playlist, intialPlayerState } from "./types";
+
+interface Audioplayer extends Controls {
+	playerState: PlayerState;
+}
+
+const useAudioPlayer: (playlist: Playlist) => Audioplayer = (
+	playlist: Playlist
+) => {
+	const [playerState, setPlayerState] =
+		useState<PlayerState>(intialPlayerState);
+	const playerRef = useRef<Controls | null>(null);
+
+	useEffect(() => {
+		const newplayer = createAudioPlayer(playlist, setPlayerState);
+		playerRef.current = newplayer;
+
+		return () => {
+			newplayer.cleanup();
+		};
+	}, [playlist]);
+
+	const togglePlayPause = () => playerRef?.current?.togglePlayPause();
+	const playNextTrack = () => playerRef?.current?.playNextTrack();
+	const playPreviousTrack = () => playerRef?.current?.playPreviousTrack();
+	const cleanup = () => playerRef?.current?.cleanup();
+
+	return {
+		playerState,
+		togglePlayPause,
+		playNextTrack,
+		playPreviousTrack,
+		cleanup,
+	};
+};
+
+export default useAudioPlayer;
